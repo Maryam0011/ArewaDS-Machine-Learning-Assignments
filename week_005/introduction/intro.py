e4b08da5a0692b5ea11f8c4afe0822f8c4262e6a
Maryam Sabo Abubakar <maryamsabubakar53@gmail.com>
2023-05-28 09:13:54 -0700

    Add files via upload

---
+++ week_005/introduction/intro.py
@@ -0,0 +1,2 @@
+print('https://docs.google.com/spreadsheets/d/1CHDYSoJcmqabdda7PrxrBAIR8PFo_ukAQb5V7j9l7s4/edit#gid=0')
+print('https://docs.google.com/document/d/1jLSUM5iWoEZlRivFHL5BWKyKQfgpCDMSglGqvn0WB8A/edit')
